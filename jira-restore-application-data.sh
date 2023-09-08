0c9f4b0832460e22c8551af690040d94c5f15398
Vladimir Mikhalev <ask@sre.gg>
2023-09-07 20:04:09 -0400

    Jira with Let's Encrypt Using Docker Compose

--- jira-restore-application-data.sh
+++ jira-restore-application-data.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-JIRA_CONTAINER=$(docker ps -aqf "name=jira_jira")
-JIRA_BACKUPS_CONTAINER=$(docker ps -aqf "name=jira_backups")
+# # jira-restore-application-data.sh Description
+# This script is designed to restore the application data.
+# 1. **Identify Containers**: Similarly to the database restore script, it identifies the service and backups containers by name.
+# 2. **List Application Data Backups**: Displays all available application data backups at the specified backup path.
+# 3. **Select Backup**: Asks the user to copy and paste the desired backup name for application data restoration.
+# 4. **Stop Service**: Stops the service to prevent any conflicts during the restore process.
+# 5. **Restore Application Data**: Removes the current application data and then extracts the selected backup to the appropriate application data path.
+# 6. **Start Service**: Restarts the service after the application data has been successfully restored.
+# To make the `jira-restore-application-data.sh` script executable, run the following command:
+# `chmod +x jira-restore-application-data.sh`
+# By utilizing this script, you can efficiently restore application data from an existing backup while ensuring proper coordination with the running service.
+
+JIRA_CONTAINER=$(docker ps -aqf "name=jira-jira")
+JIRA_BACKUPS_CONTAINER=$(docker ps -aqf "name=jira-backups")
+BACKUP_PATH="/srv/jira-application-data/backups/"
+RESTORE_PATH="/var/atlassian/application-data/jira/"
+BACKUP_PREFIX="jira-application-data"
 
 echo "--> All available application data backups:"
 
-for entry in $(docker container exec -it $JIRA_BACKUPS_CONTAINER sh -c "ls /srv/jira-application-data/backups/")
+for entry in $(docker container exec -it "$JIRA_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
 
 echo "--> Copy and paste the backup name from the list above to restore application data and press [ENTER]
---> Example: jira-application-data-backup-YYYY-MM-DD_hh-mm.tar.gz"
+--> Example: ${BACKUP_PREFIX}-backup-YYYY-MM-DD_hh-mm.tar.gz"
 echo -n "--> "
 
 read SELECTED_APPLICATION_BACKUP
@@ -19,11 +34,11 @@ read SELECTED_APPLICATION_BACKUP
 echo "--> $SELECTED_APPLICATION_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $JIRA_CONTAINER
+docker stop "$JIRA_CONTAINER"
 
 echo "--> Restoring application data..."
-docker exec -it $JIRA_BACKUPS_CONTAINER sh -c "rm -rf /var/atlassian/application-data/jira/* && tar -zxpf /srv/jira-application-data/backups/$SELECTED_APPLICATION_BACKUP -C /"
+docker exec -it "$JIRA_BACKUPS_CONTAINER" sh -c "rm -rf ${RESTORE_PATH}* && tar -zxpf ${BACKUP_PATH}${SELECTED_APPLICATION_BACKUP} -C /"
 echo "--> Application data recovery completed..."
 
 echo "--> Starting service..."
-docker start $JIRA_CONTAINER
+docker start "$JIRA_CONTAINER"
--- jira-restore-database.sh
+++ jira-restore-database.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-JIRA_CONTAINER=$(docker ps -aqf "name=jira_jira")
-JIRA_BACKUPS_CONTAINER=$(docker ps -aqf "name=jira_backups")
+# # jira-restore-database.sh Description
+# This script facilitates the restoration of a database backup.
+# 1. **Identify Containers**: It first identifies the service and backups containers by name, finding the appropriate container IDs.
+# 2. **List Backups**: Displays all available database backups located at the specified backup path.
+# 3. **Select Backup**: Prompts the user to copy and paste the desired backup name from the list to restore the database.
+# 4. **Stop Service**: Temporarily stops the service to ensure data consistency during restoration.
+# 5. **Restore Database**: Executes a sequence of commands to drop the current database, create a new one, and restore it from the selected compressed backup file.
+# 6. **Start Service**: Restarts the service after the restoration is completed.
+# To make the `jira-restore-database.shh` script executable, run the following command:
+# `chmod +x jira-restore-database.sh`
+# Usage of this script ensures a controlled and guided process to restore the database from an existing backup.
+
+JIRA_CONTAINER=$(docker ps -aqf "name=jira-jira")
+JIRA_BACKUPS_CONTAINER=$(docker ps -aqf "name=jira-backups")
+JIRA_DB_NAME="jiradb"
+JIRA_DB_USER="jiradbuser"
+POSTGRES_PASSWORD=$(docker exec $JIRA_BACKUPS_CONTAINER printenv PGPASSWORD)
+BACKUP_PATH="/srv/jira-postgres/backups/"
 
 echo "--> All available database backups:"
 
-for entry in $(docker container exec -it $JIRA_BACKUPS_CONTAINER sh -c "ls /srv/jira-postgres/backups/")
+for entry in $(docker container exec "$JIRA_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
@@ -19,13 +35,13 @@ read SELECTED_DATABASE_BACKUP
 echo "--> $SELECTED_DATABASE_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $JIRA_CONTAINER
+docker stop "$JIRA_CONTAINER"
 
 echo "--> Restoring database..."
-docker exec -it $JIRA_BACKUPS_CONTAINER sh -c 'PGPASSWORD="$(echo $POSTGRES_PASSWORD)" dropdb -h postgres -p 5432 jiradb -U jiradbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 jiradb -U jiradbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" gunzip -c /srv/jira-postgres/backups/'$SELECTED_DATABASE_BACKUP' | PGPASSWORD=$(echo $POSTGRES_PASSWORD) psql -h postgres -p 5432 jiradb -U jiradbuser'
+docker exec "$JIRA_BACKUPS_CONTAINER" sh -c "dropdb -h postgres -p 5432 $JIRA_DB_NAME -U $JIRA_DB_USER \
+&& createdb -h postgres -p 5432 $JIRA_DB_NAME -U $JIRA_DB_USER \
+&& gunzip -c ${BACKUP_PATH}${SELECTED_DATABASE_BACKUP} | psql -h postgres -p 5432 $JIRA_DB_NAME -U $JIRA_DB_USER"
 echo "--> Database recovery completed..."
 
 echo "--> Starting service..."
-docker start $JIRA_CONTAINER
+docker start "$JIRA_CONTAINER"
